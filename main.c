284e9ab9d524c54744f0522ab79b478ec33134ea
Profic0de <anonymou3489@gmail.com>
2026-05-19 14:32:11 +0300

    19/5/26

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int file_store(char* filename);
 int run_file(int idx);
 
 int main(int argc, char *argv[]) {
-    assert(("You are not using a 64 bit os"&&sizeof(size_t)==8)); // Required for lexer optimizations
+    assert(("You are not using a 64 bit version"&&sizeof(size_t)==8)); // Required for lexer optimizations
     assert(("At least one argument required",argc>1));
 
     int r = file_store(argv[argc-1]);
