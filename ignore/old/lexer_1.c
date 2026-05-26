1072c14fe5de86edcdb9f9d16780cfb1c55666ef
proficode <anonymou3489@gmail.com>
2026-05-26 20:54:48 +0300

    26/5/26

--- ignore/old/lexer_1.c
+++ ignore/old/lexer_1.c
@@ -3,20 +3,8 @@
 struct file** files;
 
 #define stack_block(type, ptr) root.blocks = array_append(root.blocks, make_block(type, ptr))
-#define FREE (free(bytes),free(buffer))
-#define ERROR(i) 100+i
-#define POS ptr-buffer
 
-int parse_fd(size_t fd) {
-    size_t size = lseek(fd, 0, SEEK_END);
-    lseek(fd, 0, SEEK_SET);
-
-    char* buffer = malloc(size), *ptr = buffer-1, *end = ptr+1; {
-        size_t len = read(fd, buffer, size);
-        if (0 >= len) return 2;
-        end += len;
-    }
-
+int parse_fd(FILE* fd) {
     static char* list = "+-/*!=%%><()[]{}&|~^;,.";
     static char operators[256];
     if (operators[(int)*list]) goto skip;
@@ -26,19 +14,18 @@ int parse_fd(size_t fd) {
 
     struct file** f = files-1; while (*++f); f -= 1;
     struct file* file = f[0];
-    #define chr ((++ptr)!=end&&(c=*ptr))
+    #define chr ((c=getc(fd))!=EOF)
 
     int c = 0;
     char* bytes = NULL;
-    while (ptr!=end) {
+    while (c!=EOF) {
         enum token_type token_type = NONE;
 
         if (!chr) break;
         if (isspace(c)) continue;
         if (c=='#') {
-            size_t val = 0, pos = 0, len = 0;
-            while (chr&&c!='\n') if (!(val&((size_t)0xFF<<(8*7)))) val = (val<<8)|c; else pos += (len++, (!pos)*(POS));
-            bytes = strndup(buffer+pos,len);
+            size_t val = 0;
+            while (chr&&c!='\n') if (!(val&((size_t)0xFF<<(8*7)))) val = (val<<8)|c; else str_append(&bytes, c);
             if (val==((size_t)'requ'<<(8*4)|'ire ')&&((c=bytes?bytes[0]:0)=='\''||c=='"'||c=='<')) {
                 char* b = bytes+1;
                 char e = bytes[0]=='<'?'>':bytes[0];
@@ -54,72 +41,50 @@ int parse_fd(size_t fd) {
 
         if (isdigit(c)) {
             token_type = NUMBER;
-            size_t start = POS, len = 1;
+            str_append(&bytes, c);
             while (chr) {
-                size_t pos = POS;
-                if (isspace(c)||(operators[c]&&c!='.')) {ptr--;break;}
-                else if (isalpha(c)) return (FREE,error(file->filename, pos, 1, "error: a number can't contain letters"), ERROR(10));
+                if (isspace(c)||(operators[c]&&c!='.')) break;
+                else if (isalpha(c)) return (free(bytes),error_message(file->filename, line, column, 1, "error: a number can't contain letters"), 1);
                 else if (c=='.'&&token_type==NUMBER) token_type = FLOAT;
-                else if (c=='.') return (FREE,error(file->filename, pos, 1, "error: a number can only have one dot"), ERROR(11));
-                len++;
+                else if (c=='.') return (free(bytes),error_message(file->filename, line, column, 1, "error: a number can only have one dot"), 1);
+                str_append(&bytes, c);
             }
-            bytes = strndup(buffer+start,len);
         } else if (operators[c]) {
             token_type = SYMBOL;
-
-            size_t start = POS;
-            size_t pos = POS;
-
-            size_t val = c;
-            size_t len = 1;
-
-            if (lookup(*(size_t*)"[]{}();,", c) || c=='.' || c=='~') {
-                ptr++;
-            } else {
-                while (chr && operators[c] && !((val = (val << 8) | c) & 0xFF000000UL)) {
-                    len++;
-                }
-            }
-
-            if (val & 0xFF000000UL)
-                return (
-                    FREE,
-                    error(file->filename, pos, 3, "error: invalid symbol"),
-                    ERROR(12)
-                );
-
-            bytes = strndup(buffer + start, len);
-            ptr--;
+            str_append(&bytes, c);
+            size_t val = c, a = column;
+            if (lookup(*(size_t*)"[]{}();,", c)||c=='.'||c=='~') c=getc(fd);
+            else while (chr&&operators[c]&&!((val=(val<<8)|c)&0xFF000000UL)) str_append(&bytes, c);
+            if (val&0xFF000000UL) return (free(bytes), error_message(file->filename, line, a, 3, "error: invalid symbol"), 1);
+            ungetc(c, fd);
         } else if (c=='\''||c=='"') {
             token_type = STRING;
-            size_t start = POS, len = 0;
+            size_t a = column;
             char b = c, p = 0, po = 0;
-            while (chr && !(c==b && !(p=='\\' && po!='\\')) && c!='\n') po = (len++, p), p = c;
-            bytes = strndup(buffer+start,len);
-            // print("%c",*++ptr);
-            if (c!=b) return (FREE, error(file->filename, start, 1, "error: string not closed"), ERROR(13));
+            str_append(&bytes, c);
+            while (chr&&c==b?(po!=p&&p=='\\'):1&&c!='\n') po = (str_append(&bytes, c), p), p = c;
+            if (c=='\n') return (free(bytes), error_message(file->filename, line-1, a, 1, "error: string not closed"), 1);
         } else {
             token_type = KEYWORD;
             char p = 0;
-            size_t pos = POS, val = c, len = 1;
-            size_t start = pos;
+            size_t col = column;
+            size_t val = c;
+            size_t len = 1;
+            str_append(&bytes, c);
             while (chr) {
                 if (!(isalnum(c)||c=='.')) {
                     if (p=='.') {
-                        if (c=='\n') ptr--;
-                        return (FREE, error(file->filename, pos, 1, "error: expected a keyword"), ERROR(14));
+                        if (c=='\n') ungetc(c, fd);
+                        return (free(bytes), error_message(file->filename, line, col+1, 1, "error: expected a keyword"), 1);
                     } break;
-                } if (p=='.'&&!isalpha(c)) return (FREE, error(file->filename, pos+1, 1, "error: invalid keyword"), ERROR(15));
+                } if (p=='.'&&!isalpha(c)) return (free(bytes), error_message(file->filename, line, col+1, 1, "error: invalid keyword"), 1);
                 if (c=='.') token_type = PATH;
-                if (p==c&&c=='.') return (FREE, error(file->filename, pos, 1, "error: expected a keyword"), ERROR(16));
-                p=c;
-                pos = POS;
+                if (p==c&&c=='.') return (free(bytes), error_message(file->filename, line, column, 1, "error: expected a keyword"), 1);
+                str_append(&bytes, (p=c));
+                col = column;
                 val = (val<<8)|c;
                 len++;
-            }
-            bytes = strndup(buffer+start,len);
-
-            if (token_type==KEYWORD&&len<=7) {
+            } if (token_type==KEYWORD&&len<=7) {
                 // break, if, while, else, return
                 size_t words[] = {
                     (size_t)'brea'<<(8*1)|'k',
@@ -136,14 +101,13 @@ int parse_fd(size_t fd) {
                 while (words[i]) if (words[i++]==val) token_type = WORD;
 
             }
-            ptr--;
+            ungetc(c, fd);
         }
 
         handle_token(&bytes, token_type);
         if (bytes) bytes = (free(bytes), NULL);
     }
     handle_token(NULL, 0);
-    free(buffer);
 
     return 0;
 }
@@ -152,12 +116,12 @@ int file_store(char* filename) {
     struct stat sb;
     if (stat(filename, &sb) == -1) {
         // perror("stat");
-        return ERROR(20);
+        return 1;
     }
 
     if (!S_ISREG(sb.st_mode)) {
         // print("%s is not a file",filename);
-        return ERROR(21);
+        return 1;
     }
 
     if (files) {
@@ -166,10 +130,10 @@ int file_store(char* filename) {
         if (*temp) return 0;
     }
 
-    size_t fd = open(filename, O_RDONLY);
+    FILE* fd = fopen(filename,"r");
     if (!fd) {
         print("failed to open %s",filename);
-        return ERROR(22);
+        return 1;
     }
 
     struct file* file = auto_free(malloc(sizeof(struct file)));
@@ -182,7 +146,7 @@ int file_store(char* filename) {
 
     auto_free(file->requirements);
 
-    close(fd);
+    fclose(fd);
     return r;
 }
 
