4512d89e3e759f0a9af2135deaccb7a5e5c77401
proficode <anonymou3489@gmail.com>
2026-05-26 21:24:20 +0300

    26/5/26

--- src/core/lexer.c
+++ src/core/lexer.c
@@ -1,13 +1,8 @@
 #include "kit.h"
 
-struct file** files;
-
-#define stack_block(type, ptr) root.blocks = array_append(root.blocks, make_block(type, ptr))
-#define FREE (free(bytes),free(buffer))
-#define ERROR(i) 100+i
 #define POS ptr-buffer
 
-int next_token(char* buffer) {
+int next_token(char* buffer, char* end) {
     static char* list = "+-/*!=%%><()[]{}&|~^;,.";
     static char operators[256];
     if (operators[(int)*list]) goto skip;
@@ -137,44 +132,6 @@ int next_token(char* buffer) {
     free(buffer);
 
     return 0;
-}
-
-int file_store(char* filename) {
-    struct stat sb;
-    if (stat(filename, &sb) == -1) {
-        perror("stat");
-        return 1;
-    }
-
-    if (!S_ISREG(sb.st_mode)) {
-        print("%s is not a file",filename);
-        return 2;
-    }
-
-    if (files) {
-        struct file** temp = files-1;
-        while (*++temp&&strcmp(temp[0]->filename,filename));
-        if (*temp) return 0;
-    }
-
-    size_t fd = open(filename, O_RDONLY);
-    if (!fd) {
-        print("failed to open %s",filename);
-        return ERROR(22);
-    }
-
-    struct file* file = auto_free(malloc(sizeof(struct file)));
-    file->filename = auto_free(strdup(filename));
-    file->requirements = NULL;
-
-    files = array_append(files, file);
-
-    int r = parse_file(fd);
-
-    auto_free(file->requirements);
-
-    close(fd);
-    return r;
 }
 
 __attribute__((destructor))
--- src/kit.h
+++ src/kit.h
@@ -25,14 +25,12 @@ int dict_append(char*** arr, char* ptr);
 void** array_append(void** arr, void* ptr);
 void error(const char* filename, size_t pos, size_t token_len, const char* fmt, ...);
 
-enum token_type {NONE,NUMBER,FLOAT,KEYWORD,SYMBOL,STRING,PATH,WORD};
+enum token_type {NONE,NUMBER,FLOAT,KEYWORD,SYMBOL,STRING,PATH,WORD,REQ};
 struct token {
     enum token_type type;
     char* buffer;
     size_t len;
 };
-
-int parse_file(size_t fd);
 
 #define lookup(size_t, c) (((0x0101010101010101*c ^ size_t) - 0x0101010101010101) & ~(0x0101010101010101*c ^ size_t) & 0x8080808080808080)
 #define array_append(arr, ptr) ((__typeof__(arr))array_append(((void**)(arr)), ((void*)(ptr))))
