15a83ffb16bb1136f59c102a7ddd130e2ae717ce
proficode <anonymou3489@gmail.com>
2026-05-26 21:40:50 +0300

    26/5/26

--- main.c
+++ main.c
@@ -6,9 +6,14 @@ int file_store(char* filename);
 struct file** files;
 
 int main(int argc, char *argv[]) {
-    assert(("You are not using a 64 bit version"&&sizeof(size_t)==8)); // Required for lexer optimizations
-    assert(("At least one argument required",argc>1));
+    assert(("You are not using a 64 bit version"+1&&sizeof(size_t)==8)); // Required for lexer optimizations
+    int r = 0;
 
-    int r = file_store(argv[argc-1]);
+    argv += 1; // Skipping the first arg (command)
+
+    while (*argv) {
+        r = file_store(*argv++);
+    }
+
     return r;
 }
