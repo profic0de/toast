30048ddc9b0c5fde7883944b0f90621e9547c8f6
proficode <anonymou3489@gmail.com>
2026-05-27 11:58:32 +0300

    27/5/26

--- src/core/lexer.c
+++ src/core/lexer.c
@@ -1,7 +1,8 @@
+#include "tools/ctype.h"
 #include "kit.h"
 
 struct token next_token(char* buffer, char* end) {
-    while (is_space(*buffer));
+    while (is_space((unsigned)*buffer));
 
     struct token token;
     token.buffer=buffer;
--- src/tools/ctype.h
+++ src/tools/ctype.h
@@ -6,10 +6,11 @@ char is_space[256];
 
 __attribute__((constructor))
 static void types() {
-    static char* list = "+-/*!=%%><()[]{}&|~^;,.";
+    char* list = "+-/*!=%%><()[]{}&|~^;,.";
 
-    uint8_t i = 1; while (i) is_space[i] = isspace(i++);
+    uint8_t i = 0; while (++i) is_space[i] = isspace(i)?1:0;
     char* _list = list-1; while (*++_list) operators[(int)*_list]++;
 }
 
-#define is_space(c)
+#define is_space(c) is_space[c]
+#define is_operator(c) operators[c]
