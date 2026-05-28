ab30d32f32b94fd9475dc428ac035ab8d992660b
proficode <anonymou3489@gmail.com>
2026-05-28 11:49:15 +0300

    28/5/26

--- src/tools/ctype.h
+++ src/tools/ctype.h
@@ -4,6 +4,7 @@
 char is_kw[256];
 char is_kws[256];
 char is_space[256];
+char is_digit[256];
 char is_operator[256];
 
 __attribute__((constructor))
@@ -12,6 +13,7 @@ static void types() {
     uint8_t i = 0;
 
     i=0; while (++i) is_space[i] = !!isspace(i);
+    i=0; while (++i) is_digit[i] = !!isdigit(i);
     i=0; while (++i) is_kws[i] = isalpha(i)||i=='_';
     i=0; while (++i) is_kw[i] = isalnum(i)||i=='_';
 
@@ -21,4 +23,5 @@ static void types() {
 #define is_kw(c) is_kw[c]
 #define is_kws(c) is_kws[c]
 #define is_space(c) is_space[c]
+#define is_digit(c) is_digit[c]
 #define is_operator(c) is_operator[c]
