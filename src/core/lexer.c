627c7fd5b595cbda886fc0a277a334c619342f28
proficode <anonymou3489@gmail.com>
2026-05-28 12:02:00 +0300

    28/5/26

--- src/core/lexer.c
+++ src/core/lexer.c
@@ -4,8 +4,12 @@
 struct token next_token(char* buffer, char* end) {
     while (is_space((unsigned char)*buffer)) buffer++;
     if (buffer==end) return (struct token){.buffer=buffer,.type=EOF,.len=0};
+    uint8_t c = *buffer;
 
-    
+    if (c=='#') {
+
+    }
+    else if (is_single(c)) return (struct token){.type=SYMBOL,.buffer=buffer,.len=1};
 
     struct token token;
     token.buffer=buffer;
