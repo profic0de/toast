0195594c2e85c8c5cf72e460cab26ac45d1cf14e
proficode <anonymou3489@gmail.com>
2026-05-28 11:47:23 +0300

    28/5/26

--- src/tools/ctype.h
+++ src/tools/ctype.h
@@ -1,16 +1,24 @@
 #include <ctype.h>
 #include <stdint.h>
 
-char operators[256];
+char is_kw[256];
+char is_kws[256];
 char is_space[256];
+char is_operator[256];
 
 __attribute__((constructor))
 static void types() {
-    char* list = "+-/*!=%%><()[]{}&|~^;,.";
+    char* operators = "+-/*!=%><()[]{}&|~^;,.";
+    uint8_t i = 0;
 
-    uint8_t i = 0; while (++i) is_space[i] = isspace(i)?1:0;
-    char* _list = list-1; while (*++_list) operators[(int)*_list]++;
+    i=0; while (++i) is_space[i] = !!isspace(i);
+    i=0; while (++i) is_kws[i] = isalpha(i)||i=='_';
+    i=0; while (++i) is_kw[i] = isalnum(i)||i=='_';
+
+    while (*operators) is_operator[(int)*operators++]++;
 }
 
+#define is_kw(c) is_kw[c]
+#define is_kws(c) is_kws[c]
 #define is_space(c) is_space[c]
-#define is_operator(c) operators[c]
+#define is_operator(c) is_operator[c]
