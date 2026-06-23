cc78d232b041c7aa750ac547b2a615b58a0e04c4
proficode <anonymou3489@gmail.com>
2026-06-23 17:30:18 +0300

    23/6/26

--- src/core/parser/func.c
+++ src/core/parser/func.c
@@ -3,8 +3,9 @@
 struct node* parse_func(struct parser* p) {
     struct node* ast = p->ast;
     if (!expect(p, IDENT)) return NULL;
-    struct node* node = auto_free(calloc(1, sizeof(struct node)));
+    struct node* node = calloc(1, sizeof(struct node));
     node->type=NODE_FUNC;
+    node->token=p->tok;
 
-    return node;
+    return auto_free(node);
 }
